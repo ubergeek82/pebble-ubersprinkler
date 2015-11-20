126486353df31449c2b4d248b522e23947cedf4c
ubergeek82 <ubergeek82@users.noreply.github.com>
2015-11-20 12:11:10 -0800

    first build with GET status
    GET /54ff6d066672524815131267/station
    GET /54ff6d066672524815131267

--- src/app.js
+++ src/app.js
@@ -4,20 +4,29 @@
  * Lex Dreitser
  */
 var ajax = require('ajax');
-var Clock = require('clock');
-var Accel = require('ui/accel');
+//var Clock = require('clock');
+//var Accel = require('ui/accel');
 var UI = require('ui');
-var Vector2 = require('vector2');
+//var Vector2 = require('vector2');
 var Vibe = require('ui/vibe');
 
 
-var DEVICE_ID = "54xxxxxxxxxxxxxxxxxxxxx";//FROM SPARKCORE IDE
-var ACCESS_TOKEN = "f75xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";//FROM SPARKCORE IDE
+var DEVICE_ID = "54ff6d066672524815131267";//FROM SPARKCORE IDE
+var ACCESS_TOKEN = "7ed9f984ddc9ad8d5de336f5a679f45763b55385";//FROM SPARKCORE IDE
 
+/**
 Accel.init();  
 Accel.on('data', function(e) {
   console.log('Just received ' + e.samples + ' from the accelerometer.');
+}); 
+*/
+
+var showResponse = new UI.Card({
+  title:'Response',
+  subtitle:'fetching....', 
+  scrollable:true
 });
+
 function DoPost(function_name,function_value){
   //make url based on function being called and device tokens
   var URL = 'https://api.spark.io/v1/devices/' + DEVICE_ID + '/' + function_name +'?access_token=' + ACCESS_TOKEN; //identify which sparkcore and function
@@ -45,16 +54,11 @@ function DoPost(function_name,function_value){
       // Failure
       console.log('Failed: ' + error.toString());
       Vibe.vibrate('long');
-  
-      
     }
   );
 }
 function DoGet(function_name){
   console.log("DoGet(): " + new Date().getTime());  
-  
-  //display card
-  
   
   //make url based on function being called and device tokens
   var URL = 'https://api.spark.io/v1/devices/' + DEVICE_ID + '/' + function_name +'?access_token=' + ACCESS_TOKEN; //identify which sparkcore and function
@@ -72,153 +76,49 @@ function DoGet(function_name){
       // Success
       console.log("Success: " + JSON.stringify(data));
       Vibe.vibrate('short');
-      // Show to user
-      
-    
+      showResponse.title('GET Response'+function_name);
+      showResponse.subtitle('Result = ' + JSON.stringify(data.result));
+      showResponse.body(JSON.stringify(data));
+      showResponse.show();
     },
     function(error) {
       // Failure
       console.log('Failed: ' + error.toString());
       Vibe.vibrate('long');
     }
+    
   );
   
   console.log("Comleted DoGet(): " + new Date().getTime());  
 }
 
 
-// Make a list of menu items
-var options = [
+var controls = [
   {
-    title: "Occupancy",
-    subtitle: "Motion and Door Sensors"
+    title: "Sprinkler Status",
+    subtitle: "Check Status of UberSprinkler System"
   },
   {
-    title: "Lights",
-    subtitle: "Control Lights"
-  },
-  {
-    title: "Garage Doors",
-    subtitle: "Control Garage Doors"
-  },
-  {
-    title: "Coffee",
-    subtitle: "Coffeemaker Control"
-  },
-  {
-    title: "Drone",
-    subtitle: "Drone Control with Accelerometer"
+    title: "Core Status",
+    subtitle: "CHeck Status of Core"
   }
-
-
 ];
 
-var optionMenu = new UI.Menu({
+
+var ubersprinklerMenu = new UI.Menu({
   sections: [{
-    title: 'IoT Control Watch',
-    items: options
+    title: 'UberSprinkler',
+    items: controls
   }]
 });
 
-var garage = [
-  {
-    title: "Right",
-    subtitle: "Main Door"
-  },
-  {
-    title: "Left",
-    subtitle: "Side Door"
-  }
-];
-var garageControl = new UI.Menu({
-  sections: [{
-    title: 'Garage Control Panel',
-    items: garage
-  }]
-});
+ubersprinklerMenu.show();
 
-var coffee = [
-  {
-    title: "Upstairs",
-    subtitle: "In Kitchen"
-  },
-  {
-    title: "Workshop",
-    subtitle: "Nitro Boosted"
-  }
-];
-var coffeeControl = new UI.Menu({
-  sections: [{
-    title: 'Coffee Control Panel',
-    items: coffee
-  }]
-});
-
-var lights = [
-  {
-    title: "Light 1",
-    subtitle: "Toggle North Light"
-  },
-  {
-    title: "Light 2",
-    subtitle: "Toggle Door Light"
-  },
-  {
-    title: "Light 3",
-    subtitle: "Toggle South Light"
-  }
-];
-var lightControl = new UI.Menu({
-  sections: [{
-    title: 'Light Control Panel',
-    items: lights
-  }]
-});
-
-optionMenu.show();
-//main.show();
-
-optionMenu.on('select', function(event) {
-  //var window = new UI.Window();  
-  
-  if(event.itemIndex ===1){
-    lightControl.show();
-  }else if(event.itemIndex ===2){
-    garageControl.show();
-  }else if(event.itemIndex ===3){
-    coffeeControl.show();
-  }
-  
-
-});
-
-// Light Control
-lightControl.on('select', function(event) {
+// Ubersprinkler Menu
+ubersprinklerMenu.on('select', function(event) {
    if(event.itemIndex ===0){
-    DoPost('ToggleLight','1');
+     DoGet('station');
    }else if(event.itemIndex ===1){
-     DoPost('ToggleLight','2');
-   }else if(event.itemIndex ===2){
-     DoPost('ToggleLight','3');
+     DoGet('');
    }
-  
-});
-
-// Garage Control
-garageControl.on('select', function(event) {
-   if(event.itemIndex ===0){
-    DoPost('ToggleGarage','1');
-   }else if(event.itemIndex ===1){
-    DoPost('ToggleGarage','2');
-   }
-  
-});
-// Coffee Control
-coffeeControl.on('select', function(event) {
-   if(event.itemIndex ===0){
-    DoPost('BrewCoffee','1');
-   }else if(event.itemIndex ===1){
-    DoPost('BrewCoffee','2');
-   }
-  
 });
